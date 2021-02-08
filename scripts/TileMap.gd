f59f0ae3dab47e54e7f078d2df435969e3de620b
Zennara <64995253+Zennara@users.noreply.github.com>
2021-02-08 05:34:45 -0500

    Added breaking tile animation

--- scripts/TileMap.gd
+++ scripts/TileMap.gd
@@ -5,14 +5,43 @@ var posy
 var cellx
 var celly
 
+var duration = 0
+var BREAK_TIME = 1
+var tile
+
 func _ready():
 	pass
 
-func _process(delta):
-	posx = get_global_mouse_position().x
-	posy = get_global_mouse_position().y
+func _physics_process(delta):
 	cellx = int(get_global_mouse_position().x / get_cell_size().x)
 	celly = int(get_global_mouse_position().y / get_cell_size().y)
 	
+	$Breaking.position = (get_global_mouse_position() - Vector2(8,8)).snapped(Vector2(16,16))
+	
 	if Input.is_action_pressed("use"):
-		set_cell(cellx,celly,-1)
+		duration += delta
+		print(duration)
+		
+		
+		if duration > 5/6.0 * BREAK_TIME:
+			$Breaking.frame = 6
+		elif duration > 4/6.0 * BREAK_TIME:
+			$Breaking.frame = 5
+		elif duration > 3/6.0 * BREAK_TIME:
+			$Breaking.frame = 4
+		elif duration > 2/6.0 * BREAK_TIME:
+			$Breaking.frame = 3
+		elif duration > 1/6.0 * BREAK_TIME:
+			$Breaking.frame = 2
+		elif duration < 1/6.0 * BREAK_TIME:
+			$Breaking.frame = 1
+		else:
+			$Breaking.frame = 0
+		
+		if duration >= BREAK_TIME:
+			set_cell(cellx, celly, 1)
+			duration = 0
+
+	if Input.is_action_just_released("use"):
+		duration = 0
+		$Breaking.frame = 0
