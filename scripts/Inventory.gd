bdc2915659a1693c729ebcc0e9c7e16d226c28a8
Zennara <64995253+Zennara@users.noreply.github.com>
2021-08-11 01:05:26 -0400

    Items can now stack, have metadata

--- scripts/Inventory.gd
+++ scripts/Inventory.gd
@@ -11,16 +11,33 @@ func _ready():
 func slot_gui_input(event: InputEvent, slot: SlotClass):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT && event.pressed:
+			#currently holding an item
 			if holding_item != null:
-				if !slot.item: #place holding item into slot
+				#empty slot
+				if !slot.item:
 					slot.putIntoSlot(holding_item)
 					holding_item = null
-				else: #swap holding item with item in slot
-					var temp_item = slot.item
-					slot.pickFromSlot()
-					temp_item.global_position = event.global_position
-					slot.putIntoSlot(holding_item)
-					holding_item = temp_item
+				#slot already contains item
+				else:
+					#different item, so swap
+					if holding_item.item_name != slot.item.item_name:
+						var temp_item = slot.item
+						slot.pickFromSlot()
+						temp_item.global_position = event.global_position
+						slot.putIntoSlot(holding_item)
+						holding_item = temp_item
+					#same item, so try to merge
+					else:
+						var stack_size = int(JsonData.item_data[slot.item.item_name]["StackSize"])
+						var able_to_add = stack_size - slot.item.item_quantity
+						if able_to_add >= holding_item.item_quantity:
+							slot.item.add_item_quantity(holding_item.item_quantity)
+							holding_item.queue_free()
+							holding_item = null
+						else:
+							slot.item.add_item_quantity(able_to_add)
+							holding_item.decrease_item_quantity(able_to_add)
+			#not holding an item
 			elif slot.item:
 				holding_item = slot.item
 				slot.pickFromSlot()
--- scripts/Item.gd
+++ scripts/Item.gd
@@ -1,7 +1,30 @@
 extends Node2D
 
+var item_name
+var item_quantity
+
 func _ready():
-	if randi()%2 == 0:
-		$TextureRect.texture = load("res://assets/items/axebloodstone.png")
+	var rand_val = randi() % 3
+	if rand_val == 0:
+		item_name = "Bloodstone Axe"
+	elif rand_val == 1:
+		item_name = "Diamond Bullet"
 	else:
-		$TextureRect.texture = load("res://assets/items/bulletdiamond.png")
+		item_name = "Stick"
+		
+	$TextureRect.texture = load("res://assets/items/" + item_name + ".png")
+	var stack_size = int(JsonData.item_data[item_name]["StackSize"])
+	item_quantity = randi() % stack_size + 1
+	
+	if stack_size == 1:
+		$Label.visible = false
+	else:
+		$Label.text = String(item_quantity)
+		
+func add_item_quantity(amount):
+	item_quantity += amount
+	$Label.text = String(item_quantity)
+	
+func decrease_item_quantity(amount):
+	item_quantity -= amount
+	$Label.text = String(item_quantity)
