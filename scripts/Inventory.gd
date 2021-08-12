112e95d850d48fd87afd058eabb8ff02e4879e42
Zennara <64995253+Zennara@users.noreply.github.com>
2021-08-12 01:28:26 -0400

    Fix bugs

--- scripts/Inventory.gd
+++ scripts/Inventory.gd
@@ -5,51 +5,65 @@ onready var inventory_slots = $GridContainer
 var holding_item = null
 
 func _ready():
-	for inv_slot in inventory_slots.get_children():
-		inv_slot.connect("gui_input", self, "slot_gui_input", [inv_slot])
+	var slots = inventory_slots.get_children()
+	for i in range(slots.size()):
+		slots[i].connect("gui_input", self, "slot_gui_input", [slots[i]])
+		slots[i].slot_index = i
 	initialize_inventory()
-		
-func slot_gui_input(event: InputEvent, slot: SlotClass):
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT && event.pressed:
-			#currently holding an item
-			if holding_item != null:
-				#empty slot
-				if !slot.item:
-					slot.putIntoSlot(holding_item)
-					holding_item = null
-				#slot already contains item
-				else:
-					#different item, so swap
-					if holding_item.item_name != slot.item.item_name:
-						var temp_item = slot.item
-						slot.pickFromSlot()
-						temp_item.global_position = event.global_position
-						slot.putIntoSlot(holding_item)
-						holding_item = temp_item
-					#same item, so try to merge
-					else:
-						var stack_size = int(JsonData.item_data[slot.item.item_name]["StackSize"])
-						var able_to_add = stack_size - slot.item.item_quantity
-						if able_to_add >= holding_item.item_quantity:
-							slot.item.add_item_quantity(holding_item.item_quantity)
-							holding_item.queue_free()
-							holding_item = null
-						else:
-							slot.item.add_item_quantity(able_to_add)
-							holding_item.decrease_item_quantity(able_to_add)
-			#not holding an item
-			elif slot.item:
-				holding_item = slot.item
-				slot.pickFromSlot()
-				holding_item.global_position = get_global_mouse_position()
-				
-func _input(event):
-	if holding_item:
-		holding_item.global_position = get_global_mouse_position()
 
 func initialize_inventory():
 	var slots = inventory_slots.get_children()
 	for i in range(slots.size()):
 		if PlayerInventory.inventory.has(i):
 			slots[i].initialize_item(PlayerInventory.inventory[i][0], PlayerInventory.inventory[i][1])
+
+func slot_gui_input(event: InputEvent, slot: SlotClass):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT && event.pressed:
+			if holding_item != null:
+				if !slot.item:
+					left_click_empty_slot(slot)
+				else:
+					if holding_item.item_name != slot.item.item_name:
+						left_click_different_item(event, slot)
+					else:
+						left_click_same_item(slot)
+			elif slot.item:
+				left_click_not_holding(slot)
+				
+func _input(event):
+	if holding_item:
+		holding_item.global_position = get_global_mouse_position()
+		
+func left_click_empty_slot(slot: SlotClass):
+	PlayerInventory.add_item_to_empty_slot(holding_item, slot)
+	slot.putIntoSlot(holding_item)
+	holding_item = null
+	
+func left_click_different_item(event: InputEvent, slot: SlotClass):
+	PlayerInventory.remove_item(slot)
+	PlayerInventory.add_item_to_empty_slot(holding_item, slot)
+	var temp_item = slot.item
+	slot.pickFromSlot()
+	temp_item.global_position = event.global_position
+	slot.putIntoSlot(holding_item)
+	holding_item = temp_item
+
+func left_click_same_item(slot: SlotClass):
+	var stack_size = int(JsonData.item_data[slot.item.item_name]["StackSize"])
+	var able_to_add = stack_size - slot.item.item_quantity
+	if able_to_add >= holding_item.item_quantity:
+		PlayerInventory.add_item_quantity(slot, holding_item.item_quantity)
+		slot.item.add_item_quantity(holding_item.item_quantity)
+		holding_item.queue_free()
+		holding_item = null
+	else:
+		PlayerInventory.add_item_quantity(slot, able_to_add)
+		slot.item.add_item_quantity(able_to_add)
+		holding_item.decrease_item_quantity(able_to_add)
+		
+func left_click_not_holding(slot: SlotClass):
+	PlayerInventory.remove_item(slot)
+	holding_item = slot.item
+	slot.pickFromSlot()
+	holding_item.global_position = get_global_mouse_position()
